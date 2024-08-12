391eb981a608ce899f91b39c650ea80ef3d1b173
Frontend JavaScript <frontendjavascript@iMac-03.local>
2024-08-12 20:30:31 +0300

    joc 2D

--- curs4/mario/src/app/archer.spec.ts
+++
@@ -1,7 +0,0 @@
-import { EnemyInstance } from './enemy-instance';
-
-describe('EnemyInstance', () => {
-  it('should create an instance', () => {
-    expect(new EnemyInstance()).toBeTruthy();
-  });
-});
--- curs4/mario/src/app/enemy-instance.ts
+++
@@ -1,9 +0,0 @@
-export class EnemyInstance {
-  type = 0;
-
-  walkSprite = 1;
-  posOx = 0;
-  width = 128;
-
-  looksRight = false;
-}
